6857e138d391b4449682e6b5eb949ef910c2c52f
sugyan <sugi1982@gmail.com>
2011-11-02 00:50:19 +0900

    draw by node-canvas

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,65 @@
-var socket = io.connect();
+$(function () {
+    var socket = io.connect();
+    var color = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    while (color.length < 6) {
+        color = '0' + color;
+    }
+
+    socket.on('change', function (data) {
+        if (data) {
+            $('#' + data.id).css('background-color', '#' + data.color);
+        }
+        var favicon = $('#favicon');
+        favicon.remove();
+        favicon.attr({ href: '/favicon?' + new Date().getTime() });
+        $('head').prepend(favicon);
+    });
+
+    // mouse
+    (function () {
+        var mouse = false;
+        var colorToHex = function (color) {
+            if (color.substr(0, 1) === '#') {
+                return color;
+            }
+            var digits = /rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+            var r = parseInt(digits[1], 10);
+            var g = parseInt(digits[2], 10);
+            var b = parseInt(digits[3], 10);
+            var res = ((r << 16) | (g << 8) | b).toString(16);
+            while (res.length < 6) {
+                res = '0' + res;
+            }
+            return res;
+        };
+        var changeColor = function (id, color) {
+            var div = $('#' + id);
+            if (color !== colorToHex(div.css('background-color'))) {
+                div.css('background-color', '#' + color);
+                socket.json.emit('change', {
+                    id: id,
+                    color: color
+                });
+            }
+        };
+        $('.cell').mousemove(function () {
+            if (mouse) {
+                changeColor($(this).attr('id'), color);
+            }
+        }).click(function () {
+            changeColor($(this).attr('id'), color);
+        });
+        $(document).mousedown(function () {
+            mouse = true;
+        });
+        $(document).mouseup(function () {
+            mouse = false;
+        });
+    }());
+
+    // color
+    $('#color').change(function () {
+        color = this.color.toString();
+    });
+    $('#color').val(color);
+});
--- server.js
+++ server.js
@@ -4,14 +4,19 @@
  */
 
 var express = require('express');
+var Canvas = require('canvas');
 var io = require('socket.io');
-var routes = require('./routes');
 
 var app = module.exports = express.createServer();
+var canvas = new Canvas(16, 16);
+var ctx = canvas.getContext('2d');
+var stage = {};
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(0, 0, 16, 16);
 
 // Configuration
 
-app.configure(function(){
+app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.bodyParser());
@@ -20,17 +25,34 @@ app.configure(function(){
     app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function () {
     app.use(express.errorHandler());
 });
 
 // Routes
 
-app.get('/', routes.index);
+app.get('/', function (req, res) {
+    console.log(stage);
+    res.render('index', {
+        title: 'Favicon colloaborator',
+        stage: stage
+    });
+});
+app.get('/favicon', function (req, res) {
+    var stream = canvas.createPNGStream();
+
+    res.contentType('image/png');
+    stream.on('data', function (chunk) {
+        res.write(chunk);
+    });
+    stream.on('end', function () {
+        res.end();
+    });
+});
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
@@ -39,5 +61,14 @@ console.log("Express server listening on port %d in %s mode", app.address().port
 
 io = io.listen(app);
 io.sockets.on('connection', function (socket) {
-    console.log('connection');
+    socket.on('change', function (data) {
+        var point = data.id.split('');
+        var row = parseInt(point[0], 16);
+        var col = parseInt(point[1], 16);
+        stage[data.id] = data.color;
+        ctx.fillStyle = '#' + data.color;
+        ctx.fillRect(col, row, 1, 1);
+        socket.broadcast.emit('change', data);
+        socket.emit('change');
+    });
 });
